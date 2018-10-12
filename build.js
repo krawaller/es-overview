92e890fffef5cc220e92ce741938a2263d90bd9f
David <david.waller@svenskaspel.se>
2018-10-12 15:06:06 +0200

    tools for local dev

--- build.js
+++ build.js
@@ -6,7 +6,26 @@ function getSourcesFromRemote() {
     fetch('https://github.com/tc39/proposals/blob/master/README.md').then(res => res.text()),
     fetch('https://github.com/tc39/proposals/blob/master/stage-0-proposals.md').then(res => res.text()),
     fetch('https://github.com/tc39/proposals/blob/master/finished-proposals.md').then(res => res.text()),
-    fetch('https://github.com/tc39/proposals/blob/master/inactive-proposals.md').then(res => res.text()),
+    fetch('https://github.com/tc39/proposals/blob/master/inactive-proposals.md').then(res => res.text())
+  ]).then(([active, zero, finished, inactive]) => ({ active, zero, finished, inactive }));
+}
+
+function writeSourcesToFiles(sources) {
+  try { fs.mkdirSync('sources'); } catch(e) {}
+  return Promise.all([
+    new Promise(resolve => fs.writeFile('sources/active.html', sources.active, resolve)),
+    new Promise(resolve => fs.writeFile('sources/zero.html', sources.zero, resolve)),
+    new Promise(resolve => fs.writeFile('sources/finished.html', sources.finished, resolve)),
+    new Promise(resolve => fs.writeFile('sources/inactive.html', sources.inactive, resolve)),
+  ]).then(() => console.log('Sources written to file in the "/sources" directory'));
+};
+
+function getSourcesFromFiles() {
+  return Promise.all([
+    new Promise(resolve => fs.readFile('sources/active.html', (err, f) => resolve(f.toString()))),
+    new Promise(resolve => fs.readFile('sources/zero.html', (err, f) => resolve(f.toString()))),
+    new Promise(resolve => fs.readFile('sources/finished.html', (err, f) => resolve(f.toString()))),
+    new Promise(resolve => fs.readFile('sources/inactive.html', (err, f) => resolve(f.toString())))
   ]).then(([active, zero, finished, inactive]) => ({ active, zero, finished, inactive }));
 }
 
@@ -110,6 +129,10 @@ function populateTemplate(template, proposals) {
   );
   return template.replace('OVERVIEWCONTENT',keys.map(key => printGroup(key, proposals[key])).join('\n'));
 }
+
+// /* for local testing, do this once: */
+// getSourcesFromRemote().then(writeSourcesToFiles);
+// /* and then use getSourcesFromFiles instead down below!
 
 getSourcesFromRemote().then(sources => {
   const proposals = getProposalGroupsFromSources(sources);
